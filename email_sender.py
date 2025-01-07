ac222993e78a65b41269c5d87e9aa771c2a725c2
Mr.Roboto <mrroboto.flamedetector@gmail.com>
2025-01-07 15:46:11 +0200

    email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,13 +1,13 @@
 import smtplib
 
-my_email = "mrroboto.flamedetector@gmail.com"
+my_email = "xxxxxxxxx@gmail.com"
 password = "xxxxxxxxxxxxxxxx"
 
 with open("fire_alarm.txt","r") as file:
     message = file.read()
 
 with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()  # This line will make the email secure(by being encrypted so nobody can actually read it)
+        connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
                             to_addrs="emailaddress",
